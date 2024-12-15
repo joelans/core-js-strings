83a9ba410335510156154cacb7e75125fcfaf825
joelans <baileyderek59@gmail.com>
2024-12-15 14:27:29 +0300

    feat: implement formatTime, reverseString and orderAlphabetically functions

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -233,8 +233,19 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  let str = '';
+  if (minutes.toString().length === 1) {
+    str += `0${minutes}:`;
+  } else {
+    str += `${minutes}:`;
+  }
+  if (seconds.toString().length === 1) {
+    str += `0${seconds}`;
+  } else {
+    str += `${seconds}`;
+  }
+  return str;
 }
 
 /**
@@ -247,8 +258,8 @@ function formatTime(/* minutes, seconds */) {
  *   reverseString('abcdef') => 'fedcba'
  *   reverseString('12345') => '54321'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  return str.split('').reverse().join('');
 }
 
 /**
@@ -262,8 +273,8 @@ function reverseString(/* str */) {
  *   orderAlphabetically('textbook') => 'bekoottx'
  *   orderAlphabetically('abc123xyz') => '123abcxyz'
  */
-function orderAlphabetically(/* str */) {
-  throw new Error('Not implemented');
+function orderAlphabetically(str) {
+  return str.split('').sort().join('');
 }
 
 /**
